65c804e018e75d75e1c76071e76abdd2c0034f17
Mats Meek Kongshaug <konmat18@EGMS.no>
2019-11-20 20:24:55 +0100

    :apple:

---
+++ SoftwareDesignExam/AppleCarPlayDecorator.cs
@@ -0,0 +1,21 @@
+﻿namespace SoftwareDesignExam
+{
+    public class AppleCarPlayDecorator : CarDecorator
+    {
+        public AppleCarPlayDecorator(ICar originalCar)
+            : base(originalCar)
+        {
+        }
+
+        public override double GetPrice()
+        {
+            return base.GetPrice() + 5_000;
+        }
+        public override string GetDescription()
+        {
+            return base.GetDescription() + ", with AppleCarPlay entertainment system";
+        }
+
+
+    }
+}
