489f6d411f391596513949f1593df934a6e3e4e0
Mats Meek Kongshaug <konmat18@EGMS.no>
2019-11-21 18:49:18 +0100

    THread

---
+++ SoftwareDesignExamTests/StockCarFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareDesignExam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignExam.Tests
+{
+    [TestClass()]
+    public class StockCarFactoryTests
+    {
+        [TestMethod()]
+        public void CreateRandomCarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
