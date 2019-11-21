afdfb956840c53b42f4b390ca4a5a9a9e5568aaa
Mats Meek Kongshaug <konmat18@EGMS.no>
2019-11-21 19:50:12 +0100

    CardealerImplemented

--- SoftwareDesignExam/Buyer.cs
+++ SoftwareDesignExam/Buyer.cs
@@ -40,7 +40,8 @@ namespace SoftwareDesignExam
                 ICar bought = _cardealership.BuyCar();
                 if (bought != null)
                 {
-                    Console.WriteLine($"{_firstName} {_lastName} bought car #{counter}, {bought.GetDescription()}");                   
+                    Console.WriteLine($"AUTOXO put up car for sale #{counter}, {bought.GetDescription()}\n");
+                    Console.WriteLine($"\t\t\t\t{_firstName} {_lastName} bought car #{counter}, {bought.GetDescription()}\n\n");                   
                 }
                 Interlocked.Increment(ref this.counter);
         }
